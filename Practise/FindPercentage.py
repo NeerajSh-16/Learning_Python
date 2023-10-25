75707b286013566e76c9dfe8be21ab4db0fd1fd8
Neeraj.Sh-16 <shneeraj.2619@gmail.com>
2023-10-25 23:40:39 +0530

    Name added

--- Practise/FindPercentage.py
+++ Practise/FindPercentage.py
@@ -1,4 +1,16 @@
+'''
+The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students.
+Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+Sample Input :
+        3
+        Krishna 67 68 69
+        Arjun 70 98 63
+        Malika 52 56 60
+        Malika
+Sample Output : 56.00
+'''
 if __name__ == '__main__':
+    # Neeraj Sharma
     n = int(input())
     student_marks = {}
     for _ in range(n):
